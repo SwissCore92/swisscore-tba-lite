c7614cf943a5fb6573097aa4d8676e6c41665ed8
Patrick Schweizer <45316968+SwissCore92@users.noreply.github.com>
2025-04-14 12:37:39 +0200

    Update __init__.py

--- swisscore_tba_lite/__init__.py
+++ swisscore_tba_lite/__init__.py
@@ -1,3 +1,13 @@
+"""
+# **SwissCore TBA Lite**
+![Python](https://img.shields.io/badge/python-3.11%2B-blue)  
+
+A very basic and lightweight asynchronous [Telegram Bot API](https://core.telegram.org/bots/api) wrapper for **python 3.11+**.  
+Main focus on core functionality without all the bloat. Just *dictionaries*.  
+
+*Read full [docs on GitHub](https://github.com/SwissCore92/swisscore-tba-lite)*
+"""
+
 from .core.base_bot import BaseBot as Bot
 from .core.logger import logger
 from .core.exceptions import RestartBotException
